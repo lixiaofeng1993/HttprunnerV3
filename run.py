07ec47fe455784a945e4746b5020ea6032f93392
lixiaofeng1993 <15235514553@163.com>
2022-07-03 17:53:50 +0800

    忽略警告

--- run.py
+++
@@ -1,7 +0,0 @@
-import os
-import pytest
-
-# pytest testsuit\params_test_yml\params_csv_test.py  --alluredir ./allure_report
-# hrun testsuit\params_test_yml
-if __name__ == '__main__':
-    pytest.main()
